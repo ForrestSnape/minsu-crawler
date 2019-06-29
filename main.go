aadee6eda35f6b754394dbd0ec0f4b11520dbc2a
forrestsnape <forrest.snape@gmail.com>
2019-06-29 12:37:47 +0800

    study

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"minsu/minsu-crawler/engine"
+	"minsu/minsu-crawler/zhenai/parser"
+)
 
 func main() {
-	fmt.Println("run")
+	engine.Run(engine.Request{
+		Url:        "https://www.xiaozhu.com/xz_web2/order/pages/landlord/orderList.html",
+		ParserFunc: parser.ParseCityList,
+	})
 }
